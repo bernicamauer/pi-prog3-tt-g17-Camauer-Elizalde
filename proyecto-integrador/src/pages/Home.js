270c6410b004c20a219bf3224cc576861745569b
bernicamauer <bcamauer@udesa.edu.ar>
2024-09-18 16:15:27 -0300

    Creación pagina populares (pt4)

--- proyecto-integrador/src/pages/Home.js
+++ proyecto-integrador/src/pages/Home.js
@@ -15,6 +15,8 @@ const Home = () => {
 
           <h2>Películas más populares</h2>
           <PeliculasGrid endPoint= "https://api.themoviedb.org/3/movie/popular?language=en-US&page=1&api_key=888daf91ec4c7d2157c8904388a1ed3e" />
+          <button><Link to="/peliculaspopulares">Ver todas</Link></button>
+
 
           </section>
         </main>
