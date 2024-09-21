c039a03a681d429fb82470515893414578c6cb96
victoriaelizalde <velizalde@udesa.edu.ar>
2024-09-21 12:27:48 -0300

    Punto 4 seccion Cartelera (uno), el formulario anda falta que ande el cargar mas

--- proyecto-integrador/src/pages/Cartelera.js
+++ proyecto-integrador/src/pages/Cartelera.js
@@ -7,7 +7,10 @@ class Cartelera extends Component {
         super();
         this.state= {
             peliculas:[],
-            isLoading: true
+            peliculasFiltradas:[],
+            filterValue: "",
+            isLoading: true,
+            page: 1 
         }
     }
 
@@ -16,10 +19,40 @@ class Cartelera extends Component {
         .then((response) => response.json())
         .then((data) => this.setState(
             {datos: this.setState({
-                peliculas:data.results,
-                isLoading:false})
+                peliculas: data.results,
+                peliculasFiltradas: data.results,
+                isLoading: false,
+                page: data.page 
+        })
             }))
         .catch(error => console.log(error));
+        
+    }
+
+    cargarMas(){
+        const nextPage = this.state.page + 1; 
+        fetch(`https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=${nextPage}&api_key=888daf91ec4c7d2157c8904388a1ed3e`)
+        .then((response) => response.json())
+        .then((data) => {
+            this.setState({
+                peliculas: this.state.peliculas.concat(data.results),
+                page: data.page + 1,
+            })
+        })
+        .catch(error => console.log(error));
+            
+    }
+
+    handleFilterChange(e){
+        const value= e.target.value;
+        this.setState({
+            filterValue: value,
+            peliculasFiltradas: this.state.peliculas.filter(pelicula => pelicula.original_title.toLowerCase().includes(value.toLowerCase()))
+        })
+    }
+
+    handleFormSubmit(){
+        console.log("se envio", this.state.peliculasFiltradas);
         
     }
     
@@ -27,7 +60,20 @@ class Cartelera extends Component {
     render() {
         return (
         <>
-       <section className='cardContainer'>
+        <form onSubmit={(e) => e.preventDefault()}>
+            <input 
+                onChange= {(e) => this.handleFilterChange(e)}
+                name="peliculaFiltro"
+                value= {this.state.filterValue}
+                placeholder="Busca una pelicula!"/>
+            <button 
+                onClick= {()=> this.handleFormSubmit()}
+                type= "submit"> Buscar </button>
+
+            
+        </form>
+
+        <section className='cardContainer'>
     {this.state.isLoading ? (
         <div className="lds-spinner">
             <div></div><div></div><div></div><div></div>
@@ -35,15 +81,19 @@ class Cartelera extends Component {
             <div></div><div></div><div></div><div></div>
         </div>
     ) : (
-        this.state.peliculas.map((pelicula, idx) => (
-            <Pelicula pelicula={pelicula} key={idx} />
-        ))
+        this.state.peliculasFiltradas.length > 0 ? (
+            this.state.peliculasFiltradas.map((pelicula, idx) => (
+                <Pelicula pelicula={pelicula} key={idx} 
+                />
+                
+            ))
+        )  : (
+            <p>No se encontraron películas</p>
+        ),
+        <button onClick={() => this.cargarMas()}> Cargar Más</button>
     )}
-</section>
-
-               
-       
-
+    </section>
+    
         </>
         )
     }
