faf6fbefeb2467827dbb43502b61a135867d13e9
victoriaelizalde <velizalde@udesa.edu.ar>
2024-09-29 15:35:17 -0300

    Media queries y link del logo a Home

--- proyecto-integrador/src/components/Navbar/Navbar.js
+++ proyecto-integrador/src/components/Navbar/Navbar.js
@@ -6,7 +6,9 @@ const Navbar= () => {
     return (
         <>
             <ul className="main-nav">
-                <li><img src="./img/logoPelicula.png" alt=""/></li>
+           
+
+                <li><Link to="/"><img src="./img/logoPelicula.png" alt="" /></Link></li>
                 <li><Link className="elemento" to="/">Home</Link></li>
                 <li><Link className="elemento"to="/favoritos">Favoritos</Link></li>
                 <li><Link className="elemento"to="/peliculaspopulares">Peliculas populares</Link></li>
